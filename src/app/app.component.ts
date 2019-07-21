29febd5e31119d754b42c231302dc990af70ee90
cadekeye <calmotuns@yahoo.co.uk>
2019-07-21 21:36:16 +0100

    some updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Caleb Adekeye';
+  Header = 'Testing';
 }
--- src/app/circular/circular.component.ts
+++ src/app/circular/circular.component.ts
@@ -5,12 +5,25 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './circular.component.html',
   styleUrls: ['./circular.component.css']
 })
+
+
 export class CircularComponent implements OnInit {
   public HappyText = 'I am happy';
 
-  constructor() { }
+  public Todo = [
+     'Running',
+     'Walking',
+     'Squarting'
+
+  ];
+  
+  constructor(){}
 
   ngOnInit() {
+  }
+
+  ChangeMe(){
+    this.HappyText="I am more happy";
   }
 
 }
